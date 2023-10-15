ec4868ff5a0cfe41d76d6d4230580cd457dd1dc3
yalefresne <yalefresne@gmail.com>
2023-10-15 18:00:55 +0200

    settup test to publish it

--- Blip/setup.py
+++ Blip/setup.py
@@ -1,18 +1,18 @@
 import setuptools
 
 setuptools.setup(
-    name="django-blip",
-    version="0.0.10",
+    name="django-blip-yale",
+    version="0.0.2",
     description="Python package to intercept all external api call during django test.",
     long_description="TEST",
     long_description_content_type="text/markdown",
-    author="Abhinav Prakash",
-    author_email="abhinavsp0730@gmail.com",
+    author="yale",
+    author_email="yalefresne@gmail.com",
     license="MIT License",
     package_dir={"": "src"},
     packages=setuptools.find_namespace_packages(where="src"),
     zip_safe=False,
-    url="https://github.com/abhinavsp0730/blip/",
+    url="https://github.com/yalefresne/blip/",
     install_requires=["httpretty>=1.1.4"],
     python_requires=">=3.9",
     classifiers=[
